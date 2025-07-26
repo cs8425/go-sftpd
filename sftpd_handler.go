b1ee27c526c23fb1559a81991d0bce383d6c4933
cs8425 <cs8425@gmail.com>
2025-07-27 03:37:40 +0800

    hide UID & GID, remove chown

--- sftpd_handler.go
+++ sftpd_handler.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/pkg/sftp"
 )
@@ -36,16 +37,54 @@ func (h *RootedHandler) cleanPath(fp string) string {
 	return newFp
 }
 
+var _ sftp.FileReader = (*RootedHandler)(nil)
+
 func (h *RootedHandler) Fileread(r *sftp.Request) (io.ReaderAt, error) {
 	path := h.cleanPath(r.Filepath)
 	Vln(3, "[Fileread]", r.Method, r.Filepath, path)
 	return h.root.Open(h.toRootPath(r.Filepath))
 }
+
+var _ sftp.FileWriter = (*RootedHandler)(nil)
+
 func (h *RootedHandler) Filewrite(r *sftp.Request) (io.WriterAt, error) {
 	path := h.cleanPath(r.Filepath)
 	Vln(3, "[Filewrite]", r.Method, r.Filepath, path)
 	return h.root.OpenFile(h.toRootPath(r.Filepath), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 }
+
+var _ sftp.OpenFileWriter = (*RootedHandler)(nil)
+
+func (h *RootedHandler) OpenFile(r *sftp.Request) (sftp.WriterAtReaderAt, error) {
+	sftpFlag := r.Pflags()
+	osFlag := 0
+	if sftpFlag.Read && sftpFlag.Write {
+		osFlag = os.O_RDWR
+	}
+	if sftpFlag.Read && !sftpFlag.Write {
+		osFlag = os.O_RDONLY
+	}
+	if !sftpFlag.Read && sftpFlag.Write {
+		osFlag = os.O_WRONLY
+	}
+	if sftpFlag.Creat {
+		osFlag |= os.O_CREATE
+	}
+	if sftpFlag.Trunc {
+		osFlag |= os.O_TRUNC
+	}
+	if sftpFlag.Excl {
+		osFlag |= os.O_EXCL
+	}
+	// WriterAt will seek
+	// if sftpFlag.Append {
+	// 	osFlag |= os.O_APPEND
+	// }
+	return h.root.OpenFile(h.toRootPath(r.Filepath), osFlag, 0644)
+}
+
+var _ sftp.FileCmder = (*RootedHandler)(nil)
+
 func (h *RootedHandler) Filecmd(r *sftp.Request) error {
 	path := h.cleanPath(r.Filepath)
 	Vln(3, "[Filecmd]", r.Method, r.Filepath, path)
@@ -72,8 +111,6 @@ func (h *RootedHandler) Filecmd(r *sftp.Request) error {
 		// r.Filepath is the target, and r.Target is the linkpath.
 		// os.Symlink(h.cleanPath(r.Filepath), h.cleanPath(r.Target))
 	case "Link":
-	// case "PosixRename":
-	// case "StatVFS":
 	default:
 	}
 	return sftp.ErrSSHFxOpUnsupported
@@ -89,9 +126,11 @@ func (h *RootedHandler) setstat(r *sftp.Request, path string) error {
 	if flags.Permissions {
 		err = os.Chmod(path, attr.FileMode())
 	}
-	if err == nil && flags.UidGid {
-		err = os.Chown(path, int(attr.UID), int(attr.GID))
-	}
+	// disable Chown
+	// TODO: config?
+	// if err == nil && flags.UidGid {
+	// 	err = os.Chown(path, int(attr.UID), int(attr.GID))
+	// }
 	if err == nil && flags.Acmodtime {
 		err = os.Chtimes(path, attr.AccessTime(), attr.ModTime())
 	}
@@ -101,7 +140,7 @@ func (h *RootedHandler) setstat(r *sftp.Request, path string) error {
 	return err
 }
 
-var _ sftp.ReadlinkFileLister = (*RootedHandler)(nil)
+var _ sftp.FileLister = (*RootedHandler)(nil)
 
 func (h *RootedHandler) Filelist(r *sftp.Request) (sftp.ListerAt, error) {
 	path := h.cleanPath(r.Filepath)
@@ -118,46 +157,114 @@ func (h *RootedHandler) Filelist(r *sftp.Request) (sftp.ListerAt, error) {
 		if err != nil {
 			return nil, err
 		}
-		return listerAt(fis), nil
+		return NewListerAt(fis), nil
 
 	case "Stat":
 		fis, err := h.root.Stat(h.toRootPath(r.Filepath))
 		if err != nil {
 			return nil, err
 		}
-		return listerAt([]os.FileInfo{fis}), nil
+		return NewListerAt([]os.FileInfo{fis}), nil
 	case "Lstat": // should call h.Lstat(r *sftp.Request) (sftp.ListerAt, error)
 		return h.Lstat(r)
 	}
 	return nil, os.ErrInvalid
 }
 
+var _ sftp.ReadlinkFileLister = (*RootedHandler)(nil)
+
 func (h *RootedHandler) Readlink(fp string) (string, error) {
 	// TODO: ensure path will be same with os.Root
+	// TODO: ensure return path not absolute and within user home(?)
 	path := h.cleanPath(fp)
 	dst, err := os.Readlink(path)
 	return dst, err
 }
+
+var _ sftp.LstatFileLister = (*RootedHandler)(nil)
 
 func (h *RootedHandler) Lstat(r *sftp.Request) (sftp.ListerAt, error) {
 	fis, err := h.root.Lstat(h.toRootPath(r.Filepath))
 	if err != nil {
 		return nil, err
 	}
-	return listerAt([]os.FileInfo{fis}), nil
+	return NewListerAt([]os.FileInfo{fis}), nil
 }
 
-type listerAt []os.FileInfo
+// pool for custom FileInfo
+var (
+	fileInfoPool = NewFileInfoPool()
+)
+
+type FileInfoPool sync.Pool
+
+func (pl *FileInfoPool) Get() *FileInfo {
+	br := ((*sync.Pool)(pl)).Get().(*FileInfo)
+	return br
+}
+
+func (pl *FileInfoPool) Put(b *FileInfo) {
+	((*sync.Pool)(pl)).Put(b)
+}
+
+func NewFileInfoPool() *FileInfoPool {
+	return (*FileInfoPool)(&sync.Pool{
+		New: func() interface{} {
+			return new(FileInfo)
+		},
+	})
+}
+
+// implement sftp.FileInfoUidGid
+var _ sftp.FileInfoUidGid = (*FileInfo)(nil)
+
+// sftp will using ModTime() Mode() Size()
+// using os.DirEntry will not provide any benefits
+// https://github.com/pkg/sftp/blob/master/ls_formatting.go#L44
+type FileInfo struct {
+	os.FileInfo
+}
+
+// currently hard code, maybe need config?
+func (fi *FileInfo) Uid() uint32 {
+	return 10032
+}
+func (fi *FileInfo) Gid() uint32 {
+	return 10032
+}
+
+type listerAt []*FileInfo
+
+func NewListerAt(ls []os.FileInfo) listerAt {
+	lis := make([]*FileInfo, 0, len(ls))
+	for _, ofi := range ls {
+		fi := fileInfoPool.Get()
+		fi.FileInfo = ofi
+		lis = append(lis, fi)
+	}
+	return lis
+}
 
 func (l listerAt) ListAt(ls []os.FileInfo, off int64) (int, error) {
 	if int(off) >= len(l) {
 		return 0, io.EOF
 	}
-	n := copy(ls, l[off:])
+	// n := copy(ls, l[off:])
+	n := 0
+	for i, fi := range l[off:] {
+		ls[i] = fi
+		n += 1
+	}
 	if n < len(ls) {
 		return n, io.EOF
 	}
 	return n, nil
+}
+func (l listerAt) Close() error {
+	for _, fi := range l {
+		fileInfoPool.Put(fi)
+	}
+	return nil
 }
 
 func customSFTPHandlers(rootDir string) (*RootedHandler, error) {

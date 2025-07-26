1f9ac7d091e10df8370e31bda016af713b4149ac
cs8425 <cs8425@gmail.com>
2025-07-27 00:34:22 +0800

    try using os.Root API

--- main.go
+++ main.go
@@ -424,41 +424,67 @@ func proxyCopy(dst io.WriteCloser, src io.ReadCloser) {
 }
 
 // RootedHandler implements sftp.FileReader, FileWriter, FileCmder, LstatFileLister(FileLister) for rootDir restriction
+// simulate as Unix file system exposed via sftp
+// the root "/" mapping to user's home directory and should NOT escape
 type RootedHandler struct {
+	root    *os.Root
 	rootDir string
 }
 
+// for os.Root API, file path should not in absolute
+// sftp.Request.Filepath should in absolute path by "github.com/pkg/sftp"
+func (h *RootedHandler) toRootPath(fp string) string {
+	if len(fp) >= 1 && fp[:1] == "/" {
+		return "." + fp // "/abs-path/to-file" => "./abs-path/to-file"
+	}
+	return fp
+}
+
+// sftp.Request.Filepath should in absolute path by "github.com/pkg/sftp"
+// map to the user's home directory as absolute path
 func (h *RootedHandler) cleanPath(fp string) string {
-	return filepath.Join(h.rootDir, filepath.Join("/", fp))
+	newFp := filepath.Join(h.rootDir, filepath.Clean("/"+fp))
+	if !strings.HasPrefix(newFp, h.rootDir) {
+		return h.rootDir // fallback to root
+	}
+	return newFp
 }
 
 func (h *RootedHandler) Fileread(r *sftp.Request) (io.ReaderAt, error) {
 	path := h.cleanPath(r.Filepath)
 	Vln(3, "[Fileread]", r.Method, r.Filepath, path)
-	return os.Open(path)
+	return h.root.Open(h.toRootPath(r.Filepath))
 }
 func (h *RootedHandler) Filewrite(r *sftp.Request) (io.WriterAt, error) {
 	path := h.cleanPath(r.Filepath)
 	Vln(3, "[Filewrite]", r.Method, r.Filepath, path)
-	return os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	return h.root.OpenFile(h.toRootPath(r.Filepath), os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 }
 func (h *RootedHandler) Filecmd(r *sftp.Request) error {
 	path := h.cleanPath(r.Filepath)
 	Vln(3, "[Filecmd]", r.Method, r.Filepath, path)
 	switch r.Method {
 	case "Setstat":
-		return h.setstat(r)
+		// TODO: ensure path will be same with os.Root
+		return h.setstat(r, path)
 	case "Rename":
+		// only in golang 1.25, not yet now
+		// h.root.Rename(r.Filepath, r.Target)
 		newPath := h.cleanPath(r.Target)
 		return os.Rename(path, newPath)
 	case "Rmdir":
-		return os.Remove(path)
+		return h.root.Remove(h.toRootPath(r.Filepath))
 	case "Remove":
-		return os.Remove(path)
+		return h.root.Remove(h.toRootPath(r.Filepath))
 	case "Mkdir":
-		return os.Mkdir(path, 0755)
+		return h.root.Mkdir(h.toRootPath(r.Filepath), 0755)
+
+	// TODO: config for enable links
 	case "Symlink":
-		// os.Symlink(s.toLocalPath(p.Targetpath), s.toLocalPath(p.Linkpath))
+		// NOTE: given a POSIX compliant signature: symlink(target, linkpath string)
+		// this makes Request.Target the linkpath, and Request.Filepath the target.
+		// r.Filepath is the target, and r.Target is the linkpath.
+		// os.Symlink(h.cleanPath(r.Filepath), h.cleanPath(r.Target))
 	case "Link":
 	// case "PosixRename":
 	// case "StatVFS":
@@ -467,13 +493,12 @@ func (h *RootedHandler) Filecmd(r *sftp.Request) error {
 	return sftp.ErrSSHFxOpUnsupported
 }
 
-func (h *RootedHandler) setstat(r *sftp.Request) error {
+func (h *RootedHandler) setstat(r *sftp.Request, path string) error {
 	attr := r.Attributes()
 	if attr == nil {
 		return nil
 	}
 	flags := r.AttrFlags()
-	path := h.cleanPath(r.Filepath)
 	var err error
 	if flags.Permissions {
 		err = os.Chmod(path, attr.FileMode())
@@ -498,7 +523,7 @@ func (h *RootedHandler) Filelist(r *sftp.Request) (sftp.ListerAt, error) {
 	// "Readlink" handle by h.Readlink(fp string) (string, error)
 	switch r.Method {
 	case "List":
-		f, err := os.Open(path)
+		f, err := h.root.Open(h.toRootPath(r.Filepath))
 		if err != nil {
 			return nil, err
 		}
@@ -510,7 +535,7 @@ func (h *RootedHandler) Filelist(r *sftp.Request) (sftp.ListerAt, error) {
 		return listerAt(fis), nil
 
 	case "Stat":
-		fis, err := os.Stat(path)
+		fis, err := h.root.Stat(h.toRootPath(r.Filepath))
 		if err != nil {
 			return nil, err
 		}
@@ -522,14 +547,14 @@ func (h *RootedHandler) Filelist(r *sftp.Request) (sftp.ListerAt, error) {
 }
 
 func (h *RootedHandler) Readlink(fp string) (string, error) {
+	// TODO: ensure path will be same with os.Root
 	path := h.cleanPath(fp)
 	dst, err := os.Readlink(path)
 	return dst, err
 }
 
 func (h *RootedHandler) Lstat(r *sftp.Request) (sftp.ListerAt, error) {
-	path := h.cleanPath(r.Filepath)
-	fis, err := os.Lstat(path)
+	fis, err := h.root.Lstat(h.toRootPath(r.Filepath))
 	if err != nil {
 		return nil, err
 	}
@@ -549,14 +574,16 @@ func (l listerAt) ListAt(ls []os.FileInfo, off int64) (int, error) {
 	return n, nil
 }
 
-func customSFTPHandlers(rootDir string) sftp.Handlers {
-	h := &RootedHandler{rootDir: rootDir}
-	return sftp.Handlers{
-		FileGet:  h,
-		FilePut:  h,
-		FileCmd:  h,
-		FileList: h,
+func customSFTPHandlers(rootDir string) (*RootedHandler, error) {
+	root, err := os.OpenRoot(rootDir)
+	if err != nil {
+		return nil, err
 	}
+	h := &RootedHandler{
+		root:    root,
+		rootDir: rootDir,
+	}
+	return h, nil
 }
 
 func handleSession(channel ssh.Channel, requests <-chan *ssh.Request, rootDir string) {
@@ -564,9 +591,19 @@ func handleSession(channel ssh.Channel, requests <-chan *ssh.Request, rootDir st
 	for req := range requests {
 		Vln(3, "[session]Received request:", req.Type, req.WantReply, req.Payload)
 		if req.Type == "subsystem" && string(req.Payload[4:]) == "sftp" {
+			h, err := customSFTPHandlers(rootDir)
+			if err != nil {
+				Vln(3, "[session]SFTP server init error", err)
+				req.Reply(false, nil)
+				continue
+			}
 			req.Reply(true, nil)
-			hs := customSFTPHandlers(rootDir)
-			rs := sftp.NewRequestServer(channel, hs, sftp.WithStartDirectory("/"))
+			rs := sftp.NewRequestServer(channel, sftp.Handlers{
+				FileGet:  h,
+				FilePut:  h,
+				FileCmd:  h,
+				FileList: h,
+			}, sftp.WithStartDirectory("/"))
 			if err := rs.Serve(); err != nil && err != io.EOF {
 				Vf(1, "[session]SFTP request server completed with error: %v", err)
 			}
